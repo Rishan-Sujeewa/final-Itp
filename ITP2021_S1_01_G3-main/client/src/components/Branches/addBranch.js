a89d88224e610ac7bf84870cf5c50273e9df5036
ChathurangiKumudumala <chathu.dvp@gmail.com>
2021-05-14 13:53:23 +0530

    frontend changes on addBranch

--- ITP2021_S1_01_G3-main/client/src/components/Branches/addBranch.js
+++ ITP2021_S1_01_G3-main/client/src/components/Branches/addBranch.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";              //handle http requests
-import { Component } from "react";
 import {Link} from 'react-router-dom';
 import '../../css/it19519364.css';
 //import Header from './Header';
@@ -36,10 +35,10 @@ export default function AddBranch(){
         <div className="container"> 
         {/* <Header/>   */}
 
-        <center><h2>Branch Management</h2></center>
+        <br/><center><h2>Branch Management</h2></center><br/>
         <Link to="/branches" className="btn it19519364-my-btn" >Branches</Link>
         <Link to="/add" className="btn it19519364-my-btn" >Create Branch</Link>
-        <Link to="/download" className="btn it19519364-my-btn" >Branch Report</Link>
+       
 
             <form onSubmit={sendData} className= "it19519364-myForm">
                 <h2>Create Branch</h2>
