17a0e7d8f72237cf06ba105570679ebe10f33c11
Upeka <uc.pathirana@gmail.com>
2021-05-14 17:59:44 +0530

    Add a PDF download facility

--- ITP2021_S1_01_G3-main/client/src/components/Employee/Employee-list.component.js
+++ ITP2021_S1_01_G3-main/client/src/components/Employee/Employee-list.component.js
@@ -4,6 +4,8 @@ import Table from 'react-bootstrap/Table';
 import EmployeeTableRow from './EmployeeTableRow';
 import '../../css/it19197760.css';
 import { Link } from "react-router-dom";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 
 export default class EmployeeList extends Component {
@@ -14,6 +16,37 @@ export default class EmployeeList extends Component {
       Employee: []
     };
   }
+
+  // PDF download 
+
+  exportPDF = () => {
+    const unit = "pt";
+    const size = "A4"; // Use A1, A2, A3 or A4
+    const orientation = "portrait"; // portrait or landscape
+
+    const marginLeft = 40;
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(15);
+
+    const title = "Employee Details";
+    const headers = [["EmployeeId", "Firstname","Last name","NIC","Position","phone Number ","Email ","Address","Branch"]];
+
+    const data = this.state.Employee.map(elt=> [elt.EmployeeId,elt.FirstName,elt.LastName,elt.NIC,elt.position,elt.phoneNo,elt.email,elt.address,elt.branch]);
+    
+
+    let content = {
+      startY: 50,
+      head: headers,
+      body: data
+    };
+
+    doc.text(title, marginLeft, 40);
+    doc.autoTable(content);
+    doc.save("employeereport.pdf")
+  }
+
+  //end 
 
   componentDidMount() {
     axios.get('http://localhost:5000/Employee/')
@@ -68,6 +101,12 @@ export default class EmployeeList extends Component {
           {this.DataTable()}
         </tbody>
       </Table>
+      <div className="IT19197760-down-link-link">
+      <center>
+    
+    <button onClick={() => this.exportPDF()}>Generate Report</button>
+    </center>
+    </div>
     </div>);
   }
 }
